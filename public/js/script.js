eabe9d458d4b74421403ef4c3e5fc3971cf14aac
tigassefred <tigasseniwe@gmail.com>
2021-04-27 14:02:55 +0000

    modification de l'utilisateur

--- public/js/script.js
+++ public/js/script.js
@@ -18,6 +18,13 @@ function SuijeVide($variable) {
     }
   }
 
+  function toggleCheck(check, action) {
+    $(check).attr('checked',action); 
+   }
+
+  $('#userModalForm').on('show.bs.modal', function(event){
+    $('#error_span_user_modal').addClass('d-none');   
+  })
   document.getElementById('userModalForm').addEventListener('submit', function(event){
      event.preventDefault();
      let form = $(this).serialize();
@@ -62,3 +69,65 @@ function SuijeVide($variable) {
        }
      });
   });
+
+  $('#ModifierUsers').on('show.bs.modal', function (event) {
+    $('#error_span_user_edit_modal').addClass('d-none');  
+        const ref = $(event.relatedTarget).attr('value')
+  
+        $.ajax({
+          type: "get",
+          url: "/employer/"+ref,
+          success: function (response) {
+               $('#edit_lastname').val(response.lastname)
+               $('#edit_firstname').val(response.firstname)
+               $('#edit_phone').val(response.phone)
+               $('#edit_ref').val(response.ref)
+          }
+        });
+  
+       $('#error_span_user_edit_modal').addClass('d-nome');  
+  })
+  
+  $('#edit_userModalForm').on('submit', function (event) {
+    event.preventDefault();
+    let form = $(this).serialize();
+    if (SuijeVide($('#edit_lastname').val())) {
+      $('#error_span_user_edit_modal').text('Vous devez préciser le nom!!')
+      $('#error_span_user_edit_modal').removeClass('d-none');
+      return 0;
+    }
+    if (SuijeVide($('#edit_firstname').val())) {
+      $('#error_span_user_edit_modal').text('Vous devez préciser le prénoms!!')
+      $('#error_span_user_edit_modal').removeClass('d-none');
+      return 0;
+    }
+    if (SuijeVide($('#edit_phone').val())) {
+      $('#error_span_user_edit_modal').text('Vous devez préciser le Numero de Telephone!!')
+      $('#error_span_user_edit_modal').removeClass('d-none');
+      return 0;
+    }
+  
+    if ($('#edit_phone').val().trim().length > 8 || $('#edit_phone').val().trim().length < 8) {
+      $('#error_span_user_edit_modal').text('Le numero de telephone doit faire exactement 8 charactères!!')
+      $('#error_span_user_edit_modal').removeClass('d-none');
+      return 0;
+    }
+  
+    if(ContientLettre($('#edit_phone').val()))
+    {
+      $('#error_span_user_modal').text('Le numero de telephone doit contenir uniquement des chiffres !!')
+      $('#error_span_user_modal').removeClass('d-none');
+      return 0;
+    }
+   
+    $.ajax({
+      type: "POST",
+      url: "/users",
+      data: form,
+      success: function (response) {
+        livewire.emit('RefreshUserList');
+        $('#ModifierUsers').modal('hide')
+      }
+    });
+  
+  })
