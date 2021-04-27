8dc011d1b0c7253f9fbb1c991cc228fdb8f8a422
tigassefred <tigasseniwe@gmail.com>
2021-04-27 13:53:17 +0000

    integration de jquery, et dynamisation du titre

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{$titre}}</title>
 </head>
 <body>
     @yield('content')
+
+
+
+    <script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
 </body>
 </html>
