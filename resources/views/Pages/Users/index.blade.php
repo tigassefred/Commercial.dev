0db47e4f5862fbf7fa6c0eadd223a7bbb59d6fcc
tigassefred <tigasseniwe@gmail.com>
2021-04-27 13:18:14 +0000

    Routage pour la gestion des employer

---
+++ resources/views/Pages/Users/index.blade.php
@@ -0,0 +1,14 @@
+@extends('Layouts.app')
+@section('content')
+    <div class="d-flex justify-content-center">
+        <button class="btn btn-outline-primary">Tableau de board</button>
+        <button class="btn btn-outline-primary ml-5">Ajouter un Employer</button>
+    </div>
+  <hr>
+  
+
+  
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('employer', [UserController::class, 'index'])->name('user.index');
