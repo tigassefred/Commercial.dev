a5fb4805b2bfff1e81bba498b2f72e83baba24c6
tigassefred <tigasseniwe@gmail.com>
2021-05-14 17:09:23 +0000

    include pour les modal de creation et de modification

---
+++ resources/views/Pages/Stockage/Modal/create_article.blade.php
@@ -0,0 +1,72 @@
+<div id="CreateArticleModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="my-modal-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="my-modal-title">Ajout d'article</h5>
+          <button class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="alert alert-danger d-none" id="errorCreate">
+   
+          </div>
+              <form action id="form_submit_stock">
+                @csrf
+                   <div class="form-group">
+                     <label for="">Nom de l'article</label>
+                     <input id="" class="form-control" type="text" name="Nom" wire:model="Nom">
+                   </div>
+ 
+                  <div class="row">
+                       <div class="form-group col-6">
+                         <label for="Prix_achat">Prix d'achat</label>
+                         <input id="Prix_achat" class="form-control" type="text" name="Prix_achat" wire:model="Prix_achat"
+                           onkeypress="isNumberKey(event)"
+                         >
+                       </div>
+ 
+                       <div class="form-group col-6">
+                         <label for="Prix_vente">Prix de vente</label>
+                         <input id="Prix_vente" class="form-control" type="text" name="Prix_vente" wire:model="Prix_vente">
+                       </div>
+                  </div>
+ 
+                  <div class="row">
+                       <div class="form-group col-6">
+                         <label for="magazin">Quantite Magazin</label>
+                         <input id="magazin" class="form-control" type="text" name="magazin" wire:model="magazin">
+                       </div>
+ 
+                       <div class="form-group col-6">
+                         <label for="boutiq">Quantite Boutique</label>
+                         <input id="boutiq" class="form-control" type="text" name="boutiq" wire:model="boutiq">
+                       </div>
+                  </div>
+ 
+ 
+                  <div class="row">
+                       <div class="form-group col-6">
+                         <label for="nbrpaquet">Quantite Par Paquet</label>
+                         <input id="nbrpaquet" class="form-control" type="text" name="nbrpaquet" wire:model="nbrpaquet">
+                       </div>
+ 
+                       <div class="form-group col-6">
+                         <label for="nbrSeuil">Quantite Seuil</label>
+                         <input id="nbrSeuil" class="form-control" type="text" name="nbrSeuil" wire:model="nbrSeuil">
+                       </div>
+                  </div>
+                    
+                 <div class="row mr-3 ">
+                      <div class="ml-auto">
+                      <button class="btn btn-primary">Enregistrer</button>
+                      <button class="btn btn-secondary"  type="button" data-dismiss="modal" aria-label="Close" >Fermer</button>
+                      </div>
+                 </div>
+              </form>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+ 
